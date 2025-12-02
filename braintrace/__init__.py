f16f071d6d251d9050973adc00e041263c0a195c
Chaoming Wang <adaduo@outlook.com>
2025-12-02 11:51:26 +0800

    Bump version to 0.1.1 and update README for project rename to BrainTrace

--- braintrace/__init__.py
+++ braintrace/__init__.py
@@ -16,8 +16,8 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = "0.1.0"
-__version_info__ = (0, 1, 0)
+__version__ = "0.1.1"
+__version_info__ = (0, 1, 1)
 
 from braintrace._etrace_algorithms import *
 from braintrace._etrace_algorithms import __all__ as _alg_all
