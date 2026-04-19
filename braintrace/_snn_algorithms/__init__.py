e6cd82b970588f5240c7a7c388d728eba6f7b49b
Chaoming Wang <wcm.brainsim@gmail.com>
2026-04-19 13:28:31 +0800

    feat: scaffold braintrace._snn_algorithms package

---
+++ braintrace/_snn_algorithms/__init__.py
@@ -0,0 +1,22 @@
+# Copyright 2026 BrainX Ecosystem Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Online learning algorithms for spiking neural networks.
+
+Paper-faithful ``ETraceVjpAlgorithm`` subclasses: EProp, OSTL, OTPE, OTTT, OSTTP.
+See ``docs/superpowers/specs/2026-04-19-snn-online-etrace-algorithms-design.md``.
+"""
+
+__all__: list = []  # populated as each class lands
